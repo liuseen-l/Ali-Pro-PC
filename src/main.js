75d4a743bf2c5d05a50bfa0034cbce23d7459886
Mr.Liu <773890440@qq.com>
2022-09-28 10:16:22 +0800

    elementui引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from '@/router'
 import 'normalize.css/normalize.css'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 // import store from './store'
 
 Vue.config.productionTip = false
