648b414346ef4074804de356b8bb48d0b7a90a97
喃望 <1911412733@qq.com>
2022-10-05 22:38:57 +0800

    修改搜索半径

--- src/components/mapView/MapCenter.vue
+++ src/components/mapView/MapCenter.vue
@@ -46,8 +46,6 @@ export default {
             .catch((err) => {
                 alert("位置信息获取失败");
             });
-        
-        
     },
     computed: {
         searchInfo() {
@@ -188,7 +186,6 @@ export default {
                 unit: "metric",
             });
             this.map.addControl(scale, "bottom-left");
-
         },
         //获取位置信息异步函数
         locationFn() {
@@ -247,9 +244,123 @@ export default {
             });
         },
 
-        addmodels(){
+        addmodels() {
+            let map=Vue.prototype.$map;
+            // parameters to ensure the model is georeferenced correctly on the map
+            // 参数，以确保模型在地图上被正确地引用
+            const modelOrigin = [120.1198, 36.006];
+            const modelAltitude = 0;
+            const modelRotate = [Math.PI / 2, 0, 0];
 
-        }
+            const modelAsMercatorCoordinate =
+                mapboxgl.MercatorCoordinate.fromLngLat(
+                    modelOrigin,
+                    modelAltitude
+                );
+
+            // transformation parameters to position, rotate and scale the 3D model onto the map
+            // 转换参数，以便在地图上定位、旋转和缩放3D模型
+            const modelTransform = {
+                translateX: modelAsMercatorCoordinate.x,
+                translateY: modelAsMercatorCoordinate.y,
+                translateZ: modelAsMercatorCoordinate.z,
+                rotateX: modelRotate[0],
+                rotateY: modelRotate[1],
+                rotateZ: modelRotate[2],
+                /* Since the 3D model is in real world meters, a scale transform needs to be
+                 * applied since the CustomLayerInterface expects units in MercatorCoordinates.
+                 */
+                scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits(),
+            };
+
+            const THREE = window.THREE;
+
+            // configuration of the custom layer for a 3D model per the CustomLayerInterface
+            const customLayer = {
+                id: "3d-model",
+                type: "custom",
+                renderingMode: "3d",
+                onAdd: function (map, gl) {
+                    this.camera = new THREE.Camera();
+                    this.scene = new THREE.Scene();
+
+                    // create two three.js lights to illuminate the model
+                    const directionalLight = new THREE.DirectionalLight(
+                        0xffffff
+                    );
+                    directionalLight.position.set(0, -70, 100).normalize();
+                    this.scene.add(directionalLight);
+
+                    const directionalLight2 = new THREE.DirectionalLight(
+                        0xffffff
+                    );
+                    directionalLight2.position.set(0, 70, 100).normalize();
+                    this.scene.add(directionalLight2);
+
+                    // use the three.js GLTF loader to add the 3D model to the three.js scene
+                    // 'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
+                    const loader = new THREE.GLTFLoader();
+                    loader.load(
+                        // './scene.gltf',
+                        "https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf",
+                        (gltf) => {
+                            this.scene.add(gltf.scene);
+                        }
+                    );
+                    this.map = map;
+
+                    // use the Mapbox GL JS map canvas for three.js
+                    this.renderer = new THREE.WebGLRenderer({
+                        canvas: map.getCanvas(),
+                        context: gl,
+                        antialias: true,
+                    });
+
+                    this.renderer.autoClear = false;
+                },
+                render: function (gl, matrix) {
+                    const rotationX = new THREE.Matrix4().makeRotationAxis(
+                        new THREE.Vector3(1, 0, 0),
+                        modelTransform.rotateX
+                    );
+                    const rotationY = new THREE.Matrix4().makeRotationAxis(
+                        new THREE.Vector3(0, 1, 0),
+                        modelTransform.rotateY
+                    );
+                    const rotationZ = new THREE.Matrix4().makeRotationAxis(
+                        new THREE.Vector3(0, 0, 1),
+                        modelTransform.rotateZ
+                    );
+
+                    const m = new THREE.Matrix4().fromArray(matrix);
+                    const l = new THREE.Matrix4()
+                        .makeTranslation(
+                            modelTransform.translateX,
+                            modelTransform.translateY,
+                            modelTransform.translateZ
+                        )
+                        .scale(
+                            new THREE.Vector3(
+                                modelTransform.scale,
+                                -modelTransform.scale,
+                                modelTransform.scale
+                            )
+                        )
+                        .multiply(rotationX)
+                        .multiply(rotationY)
+                        .multiply(rotationZ);
+
+                    this.camera.projectionMatrix = m.multiply(l);
+                    this.renderer.resetState();
+                    this.renderer.render(this.scene, this.camera);
+                    this.map.triggerRepaint();
+                },
+            };
+
+            map.on("style.load", () => {
+                map.addLayer(customLayer, "waterway-label");
+            });
+        },
     },
 };
 </script>
